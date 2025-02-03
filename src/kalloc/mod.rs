f6d4a18518f0371b6a61290bd37f9a4891e5b39c
John Turner <jturner.usa@gmail.com>
2025-02-03 09:41:32 -0500

    impl dummy global allocator

--- src/kalloc/mod.rs
+++ src/kalloc/mod.rs
@@ -1,1 +1,18 @@
+use core::{alloc::GlobalAlloc, ptr};
+
 pub mod bump;
+
+#[global_allocator]
+pub static DUMMY: Dummy = Dummy;
+
+struct Dummy;
+
+unsafe impl GlobalAlloc for Dummy {
+    unsafe fn alloc(&self, _: core::alloc::Layout) -> *mut u8 {
+        panic!()
+    }
+
+    unsafe fn dealloc(&self, _: *mut u8, _: core::alloc::Layout) {
+        panic!()
+    }
+}
