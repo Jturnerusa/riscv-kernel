47708ae9c10a5126ecefde20a82aafb83a55b647
John Turner <jturner.usa@gmail.com>
2025-02-05 22:53:15 -0500

    debugging

--- src/kalloc.rs
+++ src/kalloc.rs
@@ -1,53 +1,15 @@
+use core::ptr;
+
 use crate::PAGE_SIZE;
 
-static mut HEAD: Option<*const u8> = None;
-
-struct FreeList {
-    next: Option<*const u8>,
-}
-
-pub unsafe fn init() {
-    let heap_start = (&crate::heap_start as *const ()).addr();
-    let heap_end = (&crate::heap_end as *const ()).addr();
-
-    let heap_len = heap_end - heap_start;
-
-    let pages = heap_len / PAGE_SIZE;
-
-    let last_page = ((pages - 1) * PAGE_SIZE + heap_start) as *mut u8;
-
-    last_page.cast::<FreeList>().write(FreeList { next: None });
-
-    let mut head = last_page;
-
-    for p in (0..pages).rev() {
-        let page = (p * PAGE_SIZE + heap_start) as *mut u8;
-
-        page.cast::<FreeList>().write(FreeList { next: Some(head) });
-
-        head = page;
-    }
-
-    HEAD = Some(head);
-}
+static mut HEAD: *mut u8 = (&raw mut crate::heap_start).cast();
 
 pub fn alloc() -> Option<*mut u8> {
     unsafe {
-        match HEAD {
-            Some(page) => {
-                let free_list = page.cast::<FreeList>();
-                HEAD = (*free_list).next;
-                Some(page.cast_mut())
-            }
-            None => None,
-        }
+        let head = HEAD;
+        HEAD = head.add(PAGE_SIZE);
+        Some(head)
     }
 }
 
-pub unsafe fn dealloc(page: *mut u8) {
-    unsafe {
-        let free_list = page.cast::<FreeList>();
-        (*free_list).next = HEAD;
-        HEAD = Some(page);
-    }
-}
+pub unsafe fn dealloc(_: *mut u8) {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,96 @@
 #![no_std]
 #![allow(dead_code)]
 
+use bitflags::bitflags;
+use core::arch::asm;
+use vm::{Page, PageEntryFlag, PageTable, PhysicalAddress, VirtualAddress};
+
 mod kalloc;
 #[macro_use]
 mod kprint;
 mod sbi;
+mod vm;
 
 const PAGE_SIZE: usize = 4096;
 
 extern "C" {
-    static heap_start: ();
+    static text_start: ();
+    static text_end: ();
+    static rodata_start: ();
+    static rodata_end: ();
+    static data_start: ();
+    static data_end: ();
+    static bss_start: ();
+    static stack_top: ();
+    static mut heap_start: ();
     static heap_end: ();
+    static bss_end: ();
+}
+
+bitflags! {
+    pub struct SatpFlag: u64 {
+        const SV39 = 8 << 60;
+    }
 }
 
 #[no_mangle]
 unsafe extern "C" fn kmain() -> ! {
     kprintln!("starting kernel");
+
+    let mut table = PageTable::default();
+    map_kernel_memory(&mut table);
+    kprintln!("mapped kernel memory");
+
+    asm!(
+        "csrw satp, {}",
+        in(reg) SatpFlag::SV39.bits() | PhysicalAddress::new((&raw const table).addr()).ppn().get() as u64
+    );
+    kprintln!("enabled virtual memory");
+
     loop {}
+}
+
+unsafe fn map_kernel_memory(root: &mut PageTable) {
+    kprintln!("mapping text section");
+    identity_map_range(
+        root,
+        (&raw const text_start).addr(),
+        (&raw const text_end).addr(),
+        PageEntryFlag::READ | PageEntryFlag::EXEC,
+    );
+
+    kprintln!("mapping rodata section");
+    identity_map_range(
+        root,
+        (&raw const rodata_start).addr(),
+        (&raw const rodata_end).addr(),
+        PageEntryFlag::READ,
+    );
+
+    kprintln!("mapping data section");
+    identity_map_range(
+        root,
+        (&raw const data_start).addr(),
+        (&raw const data_end).addr(),
+        PageEntryFlag::READ | PageEntryFlag::WRITE,
+    );
+
+    kprintln!("mapping bss section");
+    identity_map_range(
+        root,
+        (&raw const bss_start).addr(),
+        (&raw const bss_end).addr(),
+        PageEntryFlag::READ | PageEntryFlag::WRITE,
+    );
+}
+
+unsafe fn identity_map_range(root: &mut PageTable, start: usize, end: usize, flags: PageEntryFlag) {
+    for p in (start..end).step_by(PAGE_SIZE) {
+        let vaddr = Page::new(VirtualAddress::new(p)).unwrap();
+        let paddr = Page::new(PhysicalAddress::new(p)).unwrap();
+
+        root.map(vaddr, paddr, flags);
+    }
 }
 
 #[panic_handler]
